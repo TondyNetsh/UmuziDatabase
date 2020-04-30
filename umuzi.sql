84c23e9e23f17eb7ff494bd2b837003c833724de
Rotondwa Netshimbupfe <59872053+TondyNetsh@users.noreply.github.com>
2020-04-30 13:02:11 +0200

    SQL Database

--- umuzi.sql
+++ umuzi.sql
@@ -101,66 +101,47 @@ UPDATE customers
 SET FirstName = "Lerato", LastName = "Mabitso" 
 WHERE CustomerID = 1;
 
-/* DELETE the record from the Customers table for customer 2 (CustomerID = 2). */
+/* 5. DELETE the record from the Customers table for customer 2 (CustomerID = 2). */
 DELETE FROM customers WHERE CustomerID = 2;
 
+/* 6. Select all unique statuses from the Orders table and get a count of the number of orders for each unique status. */
+SELECT COUNT(DISTINCT Status) FROM orders;
 
+/* 7. Return the MAXIMUM payment made on the PAYMENTS table. */
+SELECT MAX(amount) FROM payments;
 
+/* 8. Select all customers from the “Customers” table, sorted by the “Country” column. */
+SELECT * FROM customers ORDER BY Country;
 
+/* 9. Select all products with a price BETWEEN R100 and R600. */
+SELECT * FROM products
+WHERE BuyPrice BETWEEN 100 AND 600;
+/* 10. Select all fields from “Customers” where country is “Germany” AND city is “Berlin”. */
+SELECT * FROM customers
+WHERE Country = "Germany" AND City = "Berlin";
 
+/* 11. Select all fields from “Customers” where city is “Cape Town” OR “Durban”. */
+SELECT * FROM customers
+WHERE City = "Cape Town" OR City = "Durban";
 
+/* 12. Select all records from Products where the Price is GREATER than R500. */
+SELECT * FROM products WHERE BuyPrice > 500;
 
+/* 13. Return the sum of the Amounts on the Payments table. */
+SELECT SUM(amount) FROM payments;
 
+/* 14. Count the number of shipped orders in the Orders table. */
+SELECT COUNT(Status) FROM orders WHERE Status = "Shipped";
 
+/* 15. Return the average price of all Products, in Rands and in Dollars (assume the exchange rate is R12 to the Dollar). */
+SELECT AVG(BuyPrice) AS Dollars, AVG(BuyPrice * 12) AS Rands FROM products;
 
+/* 16. Using INNER JOIN create a query that selects all Payments with Customer information. */
+SELECT * FROM Payments 
+INNER JOIN Customers 
+ON Payments.CustomerID = Customers.CustomerID;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* 17. Select all products that have turnable front wheels. */
+SELECT * FROM Products
+WHERE Description 
+LIKE 'Turnable front wheels%';
